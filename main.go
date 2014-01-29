e57402616e054c963c4a40a01b48ee8947ff8f66
Michael Husmann <michaelhusmann@gmail.com>
2014-01-29 10:37:21 +0100

    added ping to htp.net

--- main.go
+++ main.go
@@ -12,31 +12,43 @@ import (
 )
 
 func main() {
-  startIp := "192.168.178.1"
-  endIp := "192.168.178.100"
+  const startIp = "192.168.178.1"
+  const endIp = "192.168.178.100"
+  const htp = "htp.net"
+
   // regex to eliminate the output IP address
   repl := regexp.MustCompile(`\d+\.\d+\.\d+\.\d+`)
   re := regexp.MustCompile(`\s`)
 
-  fmt.Printf("Prüfe alle Ips im Bereich von %s bis %s auf Erreichbarkeit\n\n",
-              startIp, endIp)
+  // first try my service provider
 
-  out0, err0 := exec.Command("fping", "-gae", startIp,
+  out0, err := exec.Command("ping", "-c1", htp).Output()
+  if err != nil {
+    log.Fatal("htp ist nicht erreichbar")
+  }
+  s := strings.SplitAfter(string(out0), "--- ")
+  fmt.Printf("%s", s[0])
+
+  out0, err = exec.Command("fping", "-gae", startIp,
                              endIp + " 2>/dev/null").Output()
-  
+
   ips := bytes.Split(out0, []byte{'\n'})
   fmt.Printf("%d Geräte gefunden\n", len(ips)-1)
+
+  var ipAndTime []string
+  var out1 []byte
+  var out2 string
   for _, ip := range(ips) {
-    ipAndTime := re.Split(string(ip), 2)
-    out1, _ := exec.Command("host", ipAndTime[0]).Output()
+    ipAndTime = re.Split(string(ip), 2)
+    out1, _ = exec.Command("host", ipAndTime[0]).Output()
     // remove ip address from string
-    out2 := repl.ReplaceAllString(string(out1), "")
+    out2 = repl.ReplaceAllString(string(out1), "")
     fmt.Printf("%-15s --> %s", ip, strings.Replace(string(out2),
                ".in-addr.arpa domain name pointer",
                "", 1))
   }
 
-  if err0 != nil {
+  if err != nil {
     log.Fatal(" --> nicht alle hosts erreichbar")
   }
 }
